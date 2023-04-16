de815c388d908903327b5b77ed6af7097bafc3be
Daniel De La Asuncion <delaasunciondaniel@gmail.com>
2023-04-16 16:12:08 -0500

    solucinar error del lenguaje 'help' dentro de treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "java", "python", "javascript", "php", "html", "css", "markdown", "bash"},
+  ensure_installed = { "c", "lua", "vim", "java", "python", "javascript", "php", "html", "css", "markdown", "bash"},
 
   sync_install = true,
   auto_install = true,
